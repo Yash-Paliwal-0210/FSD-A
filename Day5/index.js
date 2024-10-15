6a8e20cf01b386a2054381ecbad3cff98e61cfe1
Yachna02 <yachna.22b1541181@abes.ac.in>
2024-10-15 15:27:34 +0530

    day 5

---
+++ Day5/index.js
@@ -0,0 +1,20 @@
+// let x = confirm("Are you sure to logout");
+// alert(x);
+
+// let v = prompt("Enter your name", "Yash");
+// alert(v);
+
+// let diag = () =>{
+//     console.log("Welcome to arrow function in js");
+// };
+
+// diag()
+
+// let say = () => console.log("Single line arrow function");
+// say();
+
+let sum = (x,y) => {
+    return x+y;
+}
+
+console.log( sum(2,3));
